819b896b720c7d598929a6b01f303a636037ba56
Gleb Tcivie <glebtc1515@gmail.com>
2021-11-14 16:14:50 +0200

    Cleared the main from the Check.c file and added some testing for the main function in job-poster.c

--- job-poster/Database.c
+++ job-poster/Database.c
@@ -2,12 +2,9 @@
 
 int registerUser(char UserName[MAX_SIZE], char FullName[MAX_SIZE], long ID, int age, char PhoneNumber[MAX_SIZE]) {
 	FILE* infile;
-	User user;
+	int userID = getLastIdUsers() + 1;
+	User user = { userID };	// if got userID add user otherwise ID = 1
 	// Append data to the created struct user
-
-// TODO: Find the last id from the database and create a new one with the user
-// Also inline all the creation to the struct function
-
 	strcpy(user.UserName, UserName);
 	strcpy(user.FullName, FullName);
 	user.ID = ID;
@@ -20,7 +17,9 @@ int registerUser(char UserName[MAX_SIZE], char FullName[MAX_SIZE], long ID, int 
 		exit(1);
 	} else {
 		fwrite(&user, sizeof(User), 1, infile);
+		return userID;
 	}
+	return 0;
 }
 
 int checkUserName(char UserName[MAX_SIZE]) {
@@ -59,4 +58,36 @@ int checkManagerUserName(char UserName[MAX_SIZE]) {
 		fclose(infile);
 		return 0;
 	}
+}
+
+int getLastIdUsers() {
+	FILE* infile;
+	User input;
+	infile = fopen(USERS_FILENAME, "r");
+	if (infile == NULL) {
+		fprintf(stderr, "\nERROR OPENING FILE\n");
+		exit(1);
+	} else {
+		fseek(infile, -(int)sizeof(User), SEEK_END);	// go to end of file -1 User
+		fread(&input, sizeof(User), 1, infile);	// read last user
+		fclose(infile);
+		return input.UserID;
+	}
+	return 0;
+}
+
+int getLastIdManagers() {
+	FILE* infile;
+	Manager input;
+	infile = fopen(MANAGERS_FILENAME, "r");
+	if (infile == NULL) {
+		fprintf(stderr, "\nERROR OPENING FILE\n");
+		exit(1);
+	} else {
+		fseek(infile, -(int)sizeof(Manager), SEEK_END);	// go to end of file -1 Manager
+		fread(&input, sizeof(Manager), 1, infile);	// read last manager
+		fclose(infile);
+		return input.ManagerID;
+	}
+	return 0;
 }
--- job-poster/job-poster.c
+++ job-poster/job-poster.c
@@ -2,5 +2,10 @@
 
 int main() {
 	printf("Test");
+	User me;
+	int userID = registerUser("tcivie", "Gleb Tcivie", 320488398, 24, "+972543276872");
+	if (userID) {
+		printf("Registered user");
+	}
 	return 1;
 }
