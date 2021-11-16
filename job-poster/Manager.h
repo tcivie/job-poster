f66d5278ac2da82f60721ac6458c90b0c8aa48ea
Gleb Tcivie <glebtc1515@gmail.com>
2021-11-16 22:05:58 +0200

    Fixed commenting to be compatible with visual studio standarts

--- job-poster/Manager.h
+++ job-poster/Manager.h
@@ -5,20 +5,24 @@
 #include <stdio.h>
 #include <string.h>
 
-/*  Helper function to print a specific job post
-    :param post: post
-    :return: None (just print)
-*/  void view_post(Post post);
+/// <summary>
+/// Helper function to print a specific job post
+/// </summary>
+/// <param name="post"></param>
+void view_post(Post post);
 
-/*  Requiremrnt 2.4 - Function of printing all the job posts
-    :param manager: logged in manager
-    :param post: post
-    :return: None (just print)
-*/  void view_posts(Manager manager, Post post);
+/// <summary>
+/// Requiremrnt 2.4 - Function of printing all the job posts
+/// </summary>
+/// <param name="manager">logged in manager</param>
+/// <param name="post"></param>
+void view_posts(Manager manager, Post post);
 
-/*  Requiremrnt 2.2 - Function of adding a new job post
-    :return: Post*
-*/  Post* add_new_post();
+/// <summary>
+/// Requiremrnt 2.2 - Function of adding a new job post
+/// </summary>
+/// <returns></returns>
+Post* add_new_post();
 
 
 
