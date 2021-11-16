7d202c955464e6508ea415a2d41973c5d876304e
Ido Ben Noun <idobenoun@gmail.com>
2021-11-16 11:08:20 +0200

    Added manager header file Updated manager c file

---
+++ job-poster/Menager.h
@@ -0,0 +1,18 @@
+#ifndef MANAGER
+#define MANAGER
+
+/*  Helper function to print a specific job post
+    :param post: post
+    :return: None (just print)
+*/  void view_post(Post post);
+
+/*  Requiremrnt 2.4 - Function of printing all the job posts
+    :param manager: logged in manager
+    :param post: post
+    :return: None (just print)
+*/  void view_posts(Manager manager, Post post);
+
+
+
+#endif
+
