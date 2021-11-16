ab90ab10357c8ef49a19a045f1bf000cadaa7c95
Elad Sapir <elad.1999.1999@gmail.com>
2021-11-16 18:15:13 +0200

    finished function - searching posts by category

--- job-poster/connectF.c
+++ job-poster/connectF.c
@@ -1,88 +1,46 @@
+#include "stdio.h"
+#define _CRT_SECURE_NO_WARNINGS
 #include "Database.h"
+#include "Manager.h"
 #define POSTSNUM 200
+
+int printPostsByCategory();
+int printByCategories(Post* postsArray, int size, int category, int innerCategory);
+
 int printPostsByCategory()
 {
 	int choose = -1;
 	int innerChoose = 0;
-	Post* postsArray = (Post*)malloc(sizeof(Post) * 200);
-	if (postsArray == NULL)
-		exit(1);
+	int sizeArray = 0;
+	Post postsArray[POSTSNUM];
+	sizeArray = getAllPosts(postsArray);
 	while (choose != 0)
 	{
 		printf("Choose search by category:\n1-->Location\n2-->Type (Full time job/Half time job)\n3-->Profession\n0-->Back");
-		scanf("%d", &choose);
+		scanf_s("%d", &choose);
 		while (choose > 3 || choose < 0)
 		{
 			printf("Wrong choose, try again");
-			scanf("%d", &choose);
+			scanf_s("%d", &choose);
 		}
 		switch (choose)
 		{
 		case 1:
-			innerChoose = 0;
 			printf("Choose location:\n1-->North\n2-->Center\n3-->South\n0-->Back");
-			switch (innerChoose)
-			{
-			case 1:
-				//print north posts
-				break;
-			case 2:
-				//print center posts
-				break;
-			case 3:
-				//print south posts
-				break;
-			default:
-				break;
-			}
+			scanf_s("%d", &innerChoose);
+			printByCategories(postsArray, sizeArray, choose, innerChoose);
 			break;
 		case 2:
-			innerChoose = 0;
 			printf("Choose Type:\n1-->Full time job\n2-->Half time job\n0-->Back");
-			switch (innerChoose)
-			{
-			case 1:
-				//print full time posts
-				break;
-			case 2:
-				//print half time posts
-				break;
-			default:
-				break;
-			}
+			scanf_s("%d", &innerChoose);
+			printByCategories(postsArray, sizeArray, choose, innerChoose);
 			break;
 		case 3:
-			innerChoose = 0;
 			printf("Choose proffetion:\n1-->Security\n2-->Engineering\n3-->Medicine\n4-->Restaurants\n5-->Education\n6-->Public transportation\n7-->Factories\n8-->Economics\n0-->Back");
-			switch (innerChoose)
-			{
-			case 1:
-				//print north posts
-				break;
-			case 2:
-				//print center posts
-				break;
-			case 3:
-				//print south posts
-				break;
-			case 4:
-				//print south posts
-				break;
-			case 5:
-				//print south posts
-				break;
-			case 6:
-				//print south posts
-				break;
-			case 7:
-				//print south posts
-				break;
-			case 8:
-				//print south posts
-				break;
-			default:
-				break;
-			}
+			scanf_s("%d", &innerChoose);
+			printByCategories(postsArray, sizeArray, choose, innerChoose);
+			break;
+		default:
 			break;
 		}
 	}
@@ -91,20 +49,33 @@ int printPostsByCategory()
 
 int printByCategories(Post* postsArray, int size, int category, int innerCategory)
 {
-	switch (category)
+	for (int i = 0; i < size; i++)
 	{
-	case 1:
-		for (int i = 0; i < size; i++)
+		switch (category)
 		{
-			if()
+		case 1:
+		{
+			if (postsArray[i].Location == innerCategory)
+			{
+				view_post(postsArray[i]);
+			}
 		}
 		break;
-	case 2:
-		break;
-	case 3:
-		break;
-	default:
-		break;
+		case 2:
+			if (postsArray[i].Type == innerCategory)
+			{
+				view_post(postsArray[i]);
+			}
+			break;
+		case 3:
+			if (postsArray[i].Profession == innerCategory)
+			{
+				view_post(postsArray[i]);
+			}
+			break;
+		default:
+			break;
+		}
 	}
 }
 
